03ff988e1c86588941390941a0041f5204387970
Filip Masný <filip.masny@gmail.com>
2022-04-10 00:13:07 +0200

    add gql

--- src/containers/Odborky/Odborky.tsx
+++ src/containers/Odborky/Odborky.tsx
@@ -1,6 +1,24 @@
+import { useQuery } from '@apollo/client';
 import React, { useEffect, useState } from 'react';
+import gql from 'graphql-tag';
+
+const QUERY = gql`
+  {
+    program {
+      ulohy {
+        cislo_ulohy
+        text_ulohy
+        podulohy
+      }
+    }
+  }
+`;
 
 const Odborky: React.FC = (props) => {
+  const { data, loading, error } = useQuery(QUERY);
+
+  console.log(data);
+
   return <div></div>;
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'http://localhost:3001/graphql',
+  }),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
